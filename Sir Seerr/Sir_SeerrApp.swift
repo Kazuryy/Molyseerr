f96097e4116ad40d792fe6431f126126fdc94410
Kazuryy <luc.jacques72@gmail.com>
2025-12-23 16:14:31 +0100

    Initial Commit

---
+++ Sir Seerr/Sir_SeerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sir_SeerrApp.swift
+//  Sir Seerr
+//
+//  Created by Ronan Jacques on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Sir_SeerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
